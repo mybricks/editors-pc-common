0496aab7fe04f3c6a9ef5718f3255bf1dbdfdf01
liubiao03 <liubiao03@kuaishou.com>
2023-02-28 20:06:55 +0800

    feat: fix font-layout warning

--- src/style/components/fontLayout.tsx
+++ src/style/components/fontLayout.tsx
@@ -11,9 +11,9 @@ interface OptionsItem {
 const Align = ({ options, value, style, onSelect }: { options: OptionsItem[], value: string, style?: CSSProperties, onSelect?: (key: string, opt: OptionsItem) => void }) => {
   return (
     <div className={css.aligns} style={style}>
-      {options.map((option) => {
+      {options.map((option, index) => {
         return (
-          <Tooltip overlayInnerStyle={{ fontSize: 12 }} placement="bottom" title={option?.desc}>
+          <Tooltip overlayInnerStyle={{ fontSize: 12 }} placement="bottom" title={option?.desc} key={`${option.key}_${index}`}>
             <div
               className={`${css.alignItem} ${
                 value === option.key ? css.active : ''
@@ -116,11 +116,12 @@ export default ({ verticalAlign, horizontalAlign, value, onChange }: FontLayout)
       <div className={css.layoutDesc}>对齐</div>
       <div className={css.layoutContent}> 
         <Align
+          key={'horizontal'}
           value={horizontalValue}
           options={[
-            { key: HorizontalMap.left, desc: '水平居左', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm0 4h8v1H0V4zm10 4H0v1h10V8z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
-            { key: HorizontalMap.center, desc: '水平居中', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm3 4h8v1H3V4zm9 4H2v1h10V8z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
-            { key: HorizontalMap.right, desc: '水平居右', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm6 4h8v1H6V4zm8 4H4v1h10V8z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
+            { key: HorizontalMap.left, desc: '水平居左', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm0 4h8v1H0V4zm10 4H0v1h10V8z" fill="#000" stroke="none"></path></svg> },
+            { key: HorizontalMap.center, desc: '水平居中', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm3 4h8v1H3V4zm9 4H2v1h10V8z" fill="#000" stroke="none"></path></svg> },
+            { key: HorizontalMap.right, desc: '水平居右', icon: <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h14v1H0V0zm6 4h8v1H6V4zm8 4H4v1h10V8z" fill="#000" stroke="none"></path></svg> },
           ]}
           onSelect={(nextValue) => {
             setState(c => ({ ...c, ...JSON.parse(nextValue) }))
@@ -131,11 +132,12 @@ export default ({ verticalAlign, horizontalAlign, value, onChange }: FontLayout)
           <>
             <div style={{ height: 24, background: 'rgb(229, 229, 229)', width: 1 }}></div>
             <Align
+              key={'vertical'}
               value={verticalValue}
               options={[
-                { key: VerticalMap.top, desc: '垂直居上', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M15 1H1v1h14V1zM7.854 3.646L7.5 3.293l-.354.353-3 3 .708.708L7 5.207V13h1V5.207l2.146 2.147.708-.708-3-3z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
-                { key: VerticalMap.center, desc: '垂直居中', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 5.707l.354-.353 2-2-.708-.708L8 3.793V.5H7v3.293L5.854 2.646l-.708.708 2 2 .354.353zm0 3.586l.354.353 2 2-.708.708L8 11.207V14.5H7v-3.293l-1.146 1.147-.708-.708 2-2 .354-.353zM1 8h13V7H1v1z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
-                { key: VerticalMap.bottom, desc: '垂直居下', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.854 12.354l-.354.353-.354-.353-3-3 .708-.708L7 10.793V3h1v7.793l2.146-2.147.708.708-3 3zM15 14v1H1v-1h14z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg> },
+                { key: VerticalMap.top, desc: '垂直居上', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M15 1H1v1h14V1zM7.854 3.646L7.5 3.293l-.354.353-3 3 .708.708L7 5.207V13h1V5.207l2.146 2.147.708-.708-3-3z" fill="#000" stroke="none"></path></svg> },
+                { key: VerticalMap.center, desc: '垂直居中', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 5.707l.354-.353 2-2-.708-.708L8 3.793V.5H7v3.293L5.854 2.646l-.708.708 2 2 .354.353zm0 3.586l.354.353 2 2-.708.708L8 11.207V14.5H7v-3.293l-1.146 1.147-.708-.708 2-2 .354-.353zM1 8h13V7H1v1z" fill="#000" stroke="none"></path></svg> },
+                { key: VerticalMap.bottom, desc: '垂直居下', icon: <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.854 12.354l-.354.353-.354-.353-3-3 .708-.708L7 10.793V3h1v7.793l2.146-2.147.708.708-3 3zM15 14v1H1v-1h14z" fill="#000" stroke="none"></path></svg> },
               ]}
               onSelect={(nextValue) => {
                 setState(c => ({ ...c, ...JSON.parse(nextValue) }))
