610cba4f96a0797d7c2d99395516ab641a526bbd
leon-js <42924541+leon-js@users.noreply.github.com>
2023-05-30 16:47:11 +0800

    fix

--- src/style/Style.tsx
+++ src/style/Style.tsx
@@ -10,7 +10,7 @@ import { TitleMap, Sequence } from './const';
 import { useComputed, useObservable } from '@mybricks/rxui';
 
 import {
-  Size,
+  // Size,
   Padding,
   Shadow,
   TextShadow,
@@ -38,7 +38,7 @@ export class Ctx {
 }
 
 const EditorsMap: {
-  SIZE: () => JSX.Element;
+  // SIZE: () => JSX.Element;
   PADDING: () => JSX.Element;
   FONT: () => JSX.Element;
   BORDER: () => JSX.Element;
@@ -46,7 +46,7 @@ const EditorsMap: {
   BGIMAGE: () => JSX.Element;
   [key: string]: () => JSX.Element;
 } = {
-  SIZE: Size,
+  // SIZE: Size,
   PADDING: Padding,
   FONT: Font,
   BORDER: Border,
@@ -72,6 +72,7 @@ const render = ({ editConfig, projectData = {} }: EditorProps): JSX.Element => {
       } else if (typeCheck(options, 'object')) {
         const { defaultOpen = true, showTitle = true, plugins = Sequence, items, targetDom } = options
 
+        console.log('style editor targetDom: ', targetDom)
         if (targetDom) {
           val = Object.assign(getStyle(targetDom), val)
         }
@@ -300,240 +301,284 @@ function merge_Width_Height_forSize(opts: string[]) {
 
 // TODO:迁移新版style时各插件提供默认值该如何赋予
 function getStyle(element) {
-  const value = {}
-  const cssRules = []
   const computedStyle = window.getComputedStyle(element)
+  const bodyStyle = window.getComputedStyle(document.body)
+  const value = {
+    // padding
+    paddingTop: computedStyle.paddingTop,
+    paddingRight: computedStyle.paddingRight,
+    paddingBottom: computedStyle.paddingBottom,
+    paddingLeft: computedStyle.paddingLeft,
 
-  element.classList.forEach((classname) => {
-    const cssRule = getCssRule(`.${classname}`, element)
-    if (cssRule) {
-      cssRules.push(cssRule)
-    }
-  })
+    // font
+    lineHeight: computedStyle.lineHeight,
+    letterSpacing: computedStyle.letterSpacing,
+    fontSize: computedStyle.fontSize,
+    fontWeight: computedStyle.fontWeight,
+    color: computedStyle.color,
+    fontStyle: computedStyle.fontStyle,
+    fontFamily: computedStyle.fontFamily === bodyStyle.fontFamily ? '' : computedStyle.fontFamily,
+    // TODO 拆分
+    textDecoration: computedStyle.textDecorationLine === 'none' ? 'normal' : computedStyle.textDecorationLine,
 
-  cssRules.sort((f, s) => {
-    return f.selectorText.split(' ').length - s.selectorText.split(' ').length
-  })
+    // textShadow
+    textShadow: computedStyle.textShadow === 'none' ? `0px 0px 0px ${computedStyle.color}` : computedStyle.textShadow,
 
-  const cssRulesLength = cssRules.length
+    // border
+    borderWidth: `${computedStyle.borderTopWidth} ${computedStyle.borderRightWidth} ${computedStyle.borderBottomWidth} ${computedStyle.borderLeftWidth}`,
+    borderRadius: `${computedStyle.borderTopLeftRadius} ${computedStyle.borderTopRightRadius} ${computedStyle.borderBottomRightRadius} ${computedStyle.borderBottomLeftRadius}`,
+    borderStyle: `${computedStyle.borderTopStyle} ${computedStyle.borderRightStyle} ${computedStyle.borderBottomStyle} ${computedStyle.borderLeftStyle}`,
+    borderColor: `${computedStyle.borderTopColor.replace(/\s/g, "")} ${computedStyle.borderRightColor.replace(/\s/g, "")} ${computedStyle.borderBottomColor.replace(/\s/g, "")} ${computedStyle.borderLeftColor.replace(/\s/g, "")}`,
 
-  cssRules.forEach((rule, index) => {
-    const isLastCssRule = cssRulesLength === index + 1
-    const { style } = rule
-    const {
-      // padding
-      paddingTop,
-      paddingRight,
-      paddingBottom,
-      paddingLeft,
+    // bgcolor
+    background: computedStyle.background, // 'url() center top / 100% 100% no-repeat, rgba(255,255,255,1)'
 
-      // size
-      height,
-      width,
-
-      //font
-      lineHeight,
-      letterSpacing,
-      fontSize,
-      fontWeight,
-      color,
-      fontStyle,
-      fontFamily,
-      textDecoration,
-
-      //textShadow
-      textShadow,
-
-      // border
-      borderWidth,
-      borderRadius,
-      borderStyle,
-      borderColor,
-
-      // bgcolor
-      background,
-
-      // shadow
-      boxShadow
-    } = style
-
-    if (paddingTop) {
-      value.paddingTop = paddingTop
-    }
-    if (paddingRight) {
-      value.paddingRight = paddingRight
-    }
-    if (paddingBottom) {
-      value.paddingBottom = paddingBottom
-    }
-    if (paddingLeft) {
-      value.paddingLeft = paddingLeft
-    }
-
-    if (height) {
-      value.height = height
-    }
-    if (width) {
-      value.width = width
-    }
-
-    if (lineHeight) {
-      value.lineHeight = lineHeight
-    }
-    if (letterSpacing) {
-      value.letterSpacing = letterSpacing
-    }
-    if (fontSize) {
-      value.fontSize = fontSize
-    }
-    if (fontWeight) {
-      value.fontWeight = fontWeight
-    }
-    if (color) {
-      value.color = color
-    }
-    if (fontStyle) {
-      value.fontStyle = fontStyle
-    }
-    if (fontFamily) {
-      value.fontFamily = fontFamily
-    }
-    if (textDecoration) {
-      value.textDecoration = textDecoration
-    }
-
-    if (textShadow) {
-      value.textShadow = textShadow
-    }
-
-    if (borderWidth) {
-      // value.borderWidth = borderWidth
-      value.borderWidth = `${style.borderTopWidth} ${style.borderRightWidth} ${style.borderBottomWidth} ${style.borderLeftWidth}`
-    }
-    if (borderRadius) {
-      // value.borderRadius = borderRadius
-      value.borderRadius = `${style.borderTopLeftRadius} ${style.borderTopRightRadius} ${style.borderBottomRightRadius} ${style.borderBottomLeftRadius}`
-    }
-    if (borderStyle) {
-      // value.borderStyle = borderStyle
-      value.borderStyle = `${style.borderTopStyle} ${style.borderRightStyle} ${style.borderBottomStyle} ${style.borderLeftStyle}`
-    }
-    if (borderColor) {
-      // value.borderColor = borderColor
-      value.borderColor = `${style.borderTopColor.replace(/\s/g, "")} ${style.borderRightColor.replace(/\s/g, "")} ${style.borderBottomColor.replace(/\s/g, "")} ${style.borderLeftColor.replace(/\s/g, "")}`
-    }
-
-    if (background) {
-      value.background = background
-    }
-
-    if (boxShadow) {
-      value.boxShadow = boxShadow
-    }
-      
-    if (isLastCssRule) {
-      if (!value.paddingTop) {
-        // 0
-        value.paddingTop = '0px'
-      }
-      if (!value.paddingRight) {
-        // 0
-        value.paddingRight = '0px'
-      }
-      if (!value.paddingBottom) {
-        // 0
-        value.paddingBottom = '0px'
-      }
-      if (!value.paddingLeft) {
-        // 0
-        value.paddingLeft = '0px'
-      }
-
-      if (!value.height) {
-        // auto
-        value.height = 'auto'
-      }
-      if (!value.width) {
-        // auto
-        value.width = 'auto'
-      }
-
-      // TODO: 目前只支持px，不支持倍数
-      if (!value.lineHeight || typeof value.lineHeight === 'number') {
-        // normal
-        value.lineHeight = computedStyle.lineHeight
-      }
-      if (!value.letterSpacing) {
-        // normal
-        value.letterSpacing = computedStyle.letterSpacing
-      }
-      if (!value.fontSize) {
-        value.fontSize = computedStyle.fontSize
-      }
-      if (!value.fontWeight) {
-        // normal
-        value.fontWeight = computedStyle.fontWeight
-      }
-      if (!value.color) {
-        value.color = computedStyle.color
-      }
-      if (!value.fontStyle) {
-        // normal
-        value.fontStyle = computedStyle.fontStyle
-      }
-      if (!value.fontFamily) {
-        // 用户定义
-        value.fontFamily = ''
-      }
-      if (!value.textDecoration) {
-        /**
-         * text-decoration-color:currentcolor
-            text-decoration-style:solid
-            text-decoration-line:none
-         */
-        value.textDecoration = 'normal'
-      }
-
-      // TODO:应该拿到当前文字颜色
-      if (!value.textShadow) { // none
-        value.textShadow = `0px 0px 0px ${value.color}`
-      }
-
-      // TODO:现在border默认展开有问题
-      if (!value.borderWidth) {
-        // 上下左右0
-        value.borderWidth = '0px'
-      }
-      if (!value.borderRadius) {
-        // 上下左右0
-        value.borderRadius = '0px'
-      }
-      if (!value.borderStyle) {
-        // 上下左右null
-        value.borderStyle = 'solid'
-      }
-      if (!value.borderColor) {
-        // 上下左右currentColor，给白色吧
-        value.borderColor = '#ffffff'
-      }
+    // shadow
+    boxShadow: computedStyle.boxShadow === 'none' ? '0px 0px 0px 0px #ffffff' : computedStyle.boxShadow
+  }
 
 
-      if (!value.background) {
-        // TODO:后面不用background了
-        value.background = 'url() center top / 100% 100% no-repeat, rgba(255,255,255,1)'
-        // value.background = computedStyle.background
-      }
 
-      if (!value.boxShadow) {
-        // none
-        value.boxShadow = '0px 0px 0px 0px #ffffff'
-      }
 
-    }
-  })
+
 
   return value
 }
+// function getStyle(element) {
+//   const value = {}
+//   const cssRules = []
+//   const computedStyle = window.getComputedStyle(element)
+
+//   element.classList.forEach((classname) => {
+//     const cssRule = getCssRule(`.${classname}`, element)
+//     if (cssRule) {
+//       cssRules.push(cssRule)
+//     }
+//   })
+
+//   cssRules.sort((f, s) => {
+//     return f.selectorText.split(' ').length - s.selectorText.split(' ').length
+//   })
+
+//   const cssRulesLength = cssRules.length
+
+//   cssRules.forEach((rule, index) => {
+//     const isLastCssRule = cssRulesLength === index + 1
+//     const { style } = rule
+//     const {
+//       // padding
+//       paddingTop,
+//       paddingRight,
+//       paddingBottom,
+//       paddingLeft,
+
+//       // size
+//       // height,
+//       // width,
+
+//       //font
+//       lineHeight,
+//       letterSpacing,
+//       fontSize,
+//       fontWeight,
+//       color,
+//       fontStyle,
+//       fontFamily,
+//       textDecoration,
+
+//       //textShadow
+//       textShadow,
+
+//       // border
+//       borderWidth,
+//       borderRadius,
+//       borderStyle,
+//       borderColor,
+
+//       // bgcolor
+//       background,
+
+//       // shadow
+//       boxShadow
+//     } = style
+
+//     if (paddingTop) {
+//       value.paddingTop = paddingTop
+//     }
+//     if (paddingRight) {
+//       value.paddingRight = paddingRight
+//     }
+//     if (paddingBottom) {
+//       value.paddingBottom = paddingBottom
+//     }
+//     if (paddingLeft) {
+//       value.paddingLeft = paddingLeft
+//     }
+
+//     // if (height) {
+//     //   value.height = height
+//     // }
+//     // if (width) {
+//     //   value.width = width
+//     // }
+
+//     if (lineHeight) {
+//       value.lineHeight = lineHeight
+//     }
+//     if (letterSpacing) {
+//       value.letterSpacing = letterSpacing
+//     }
+//     if (fontSize) {
+//       value.fontSize = fontSize
+//     }
+//     if (fontWeight) {
+//       value.fontWeight = fontWeight
+//     }
+//     if (color) {
+//       value.color = color
+//     }
+//     if (fontStyle) {
+//       value.fontStyle = fontStyle
+//     }
+//     if (fontFamily) {
+//       value.fontFamily = fontFamily
+//     }
+//     if (textDecoration) {
+//       value.textDecoration = textDecoration
+//     }
+
+//     if (textShadow) {
+//       value.textShadow = textShadow
+//     }
+
+//     if (borderWidth) {
+//       // value.borderWidth = borderWidth
+//       value.borderWidth = `${style.borderTopWidth} ${style.borderRightWidth} ${style.borderBottomWidth} ${style.borderLeftWidth}`
+//     }
+//     if (borderRadius) {
+//       // value.borderRadius = borderRadius
+//       value.borderRadius = `${style.borderTopLeftRadius} ${style.borderTopRightRadius} ${style.borderBottomRightRadius} ${style.borderBottomLeftRadius}`
+//     }
+//     if (borderStyle) {
+//       // value.borderStyle = borderStyle
+//       value.borderStyle = `${style.borderTopStyle} ${style.borderRightStyle} ${style.borderBottomStyle} ${style.borderLeftStyle}`
+//     }
+//     if (borderColor) {
+//       // value.borderColor = borderColor
+//       value.borderColor = `${style.borderTopColor.replace(/\s/g, "")} ${style.borderRightColor.replace(/\s/g, "")} ${style.borderBottomColor.replace(/\s/g, "")} ${style.borderLeftColor.replace(/\s/g, "")}`
+//     }
+
+//     if (background) {
+//       value.background = background
+//     }
+
+//     if (boxShadow) {
+//       value.boxShadow = boxShadow
+//     }
+      
+//     if (isLastCssRule) {
+//       if (!value.paddingTop) {
+//         // 0
+//         value.paddingTop = '0px'
+//       }
+//       if (!value.paddingRight) {
+//         // 0
+//         value.paddingRight = '0px'
+//       }
+//       if (!value.paddingBottom) {
+//         // 0
+//         value.paddingBottom = '0px'
+//       }
+//       if (!value.paddingLeft) {
+//         // 0
+//         value.paddingLeft = '0px'
+//       }
+
+//       // if (!value.height) {
+//       //   // auto
+//       //   value.height = 'auto'
+//       // }
+//       // if (!value.width) {
+//       //   // auto
+//       //   value.width = 'auto'
+//       // }
+
+//       // TODO: 目前只支持px，不支持倍数
+//       if (!value.lineHeight || typeof value.lineHeight === 'number') {
+//         // normal
+//         value.lineHeight = computedStyle.lineHeight
+//       }
+//       if (!value.letterSpacing) {
+//         // normal
+//         value.letterSpacing = computedStyle.letterSpacing
+//       }
+//       if (!value.fontSize) {
+//         value.fontSize = computedStyle.fontSize
+//       }
+//       if (!value.fontWeight) {
+//         // normal
+//         value.fontWeight = computedStyle.fontWeight
+//       }
+//       if (!value.color) {
+//         value.color = computedStyle.color
+//       }
+//       if (!value.fontStyle) {
+//         // normal
+//         value.fontStyle = computedStyle.fontStyle
+//       }
+//       if (!value.fontFamily) {
+//         // 用户定义
+//         value.fontFamily = ''
+//       }
+//       if (!value.textDecoration) {
+//         /**
+//          * text-decoration-color:currentcolor
+//             text-decoration-style:solid
+//             text-decoration-line:none
+//          */
+//         value.textDecoration = 'normal'
+//       }
+
+//       // TODO:应该拿到当前文字颜色
+//       if (!value.textShadow) { // none
+//         value.textShadow = `0px 0px 0px ${value.color}`
+//       }
+
+//       // TODO:现在border默认展开有问题
+//       if (!value.borderWidth) {
+//         // 上下左右0
+//         value.borderWidth = '0px'
+//       }
+//       if (!value.borderRadius) {
+//         // 上下左右0
+//         value.borderRadius = '0px'
+//       }
+//       if (!value.borderStyle) {
+//         // 上下左右null
+//         value.borderStyle = 'solid'
+//       }
+//       if (!value.borderColor) {
+//         // 上下左右currentColor，给白色吧
+//         value.borderColor = '#ffffff'
+//       }
+
+
+//       if (!value.background) {
+//         // TODO:后面不用background了
+//         value.background = 'url() center top / 100% 100% no-repeat, rgba(255,255,255,1)'
+//         // value.background = computedStyle.background
+//       }
+
+//       if (!value.boxShadow) {
+//         // none
+//         value.boxShadow = '0px 0px 0px 0px #ffffff'
+//       }
+
+//     }
+//   })
+
+//   return value
+// }
 
 function getCssRule(selector: string, element: HTMLDivElement) {
   for (let i = 0; i < document.styleSheets.length; i++) {
