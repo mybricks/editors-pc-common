1f58dc76d58f0b862f9c19f37c5f7a59712070cc
zouyongsheng <zouyongsheng@kuaishou.com>
2023-09-20 11:52:04 +0800

    feat: 替换静态资源

--- src/align/index.tsx
+++ src/align/index.tsx
@@ -9,19 +9,19 @@ const rowList = [
     title: "左对齐",
     key: "left",
     value: "left",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/left.defc4a63ebe8ea7d.svg",
+    url: "./mfs/editor_assets/align/left.defc4a63ebe8ea7d.svg",
   },
   {
     title: "左右居中对齐",
     key: "row-center",
     value: "center",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/center.c284343a9ff9672a.svg",
+    url: "./mfs/editor_assets/align/center.c284343a9ff9672a.svg",
   },
   {
     title: "右对齐",
     key: "right",
     value: "right",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/right.a7763b38b84b5894.svg",
+    url: "./mfs/editor_assets/align/right.a7763b38b84b5894.svg",
   },
 ];
 
@@ -30,19 +30,19 @@ const columnList = [
     title: "顶部对齐",
     key: "top",
     value: "top",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/top.98906024d52b69de.svg",
+    url: "./mfs/editor_assets/align/top.98906024d52b69de.svg",
   },
   {
     title: "上下居中对齐",
     key: "column-center",
     value: "center",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/center.100376f4ade480cd.svg",
+    url: "./mfs/editor_assets/align/center.100376f4ade480cd.svg",
   },
   {
     title: "底部对齐",
     key: "bottom",
     value: "bottom",
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/bottom.6ee532067ed440ca.svg",
+    url: "./mfs/editor_assets/align/bottom.6ee532067ed440ca.svg",
   },
 ];
 
@@ -51,13 +51,13 @@ const spaceBetweenList = [
     title: "垂直间距等分",
     key: "column",
     value: ["column", "space-between"],
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/column-space-between.31d560c0e611198f.svg",
+    url: "./mfs/editor_assets/align/column-space-between.31d560c0e611198f.svg",
   },
   {
     title: "水平间距等分",
     key: "row",
     value: ["row", "space-between"],
-    url: "https://ali-ec.static.yximgs.com/udata/pkg/eshop/fangzhou/icons/row-space-between.ead5cd660c0f1c33.svg",
+    url: "./mfs/editor_assets/align/row-space-between.ead5cd660c0f1c33.svg",
   },
 ];
 
