3df5d6dd6a57e44204cc2058feb196080f5afdba
zhuba-Ahhh <3477826311@qq.com>
2024-04-22 21:36:25 +0800

    chore(Background): change refresh icon

--- src/style_new/StyleEditor/plugins/Background/index.tsx
+++ src/style_new/StyleEditor/plugins/Background/index.tsx
@@ -81,8 +81,21 @@ export function Background ({value, onChange, config}: BackgroundProps) {
             upload={context.upload}
           />
         }
-        <div className={css.icon} data-mybricks-tip={'重置'} onClick={refresh}>
-          <ReloadOutlined />
+        <div className={css.icon} data-mybricks-tip={`{content:'重置背景',position:'left'}`} onClick={refresh}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="6"
+            viewBox="0 0 12 6"
+          >
+            <path
+              fill="#000"
+              fill-opacity="1"
+              fill-rule="nonzero"
+              stroke="none"
+              d="M11.5 3.5H.5v-1h11v1z"
+            ></path>
+          </svg>
         </div>
       </Panel.Content>
       {/* {disableBackgroundImage ? null : (
