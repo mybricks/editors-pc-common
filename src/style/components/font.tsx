e8e1601a97e981cee3e1f5ea17a59b07d413a0e8
zhuba-Ahhh <3477826311@qq.com>
2024-05-07 16:50:37 +0800

    refactor(style): font

--- src/style/components/font.tsx
+++ src/style/components/font.tsx
@@ -1,15 +1,31 @@
-import React, {CSSProperties, useMemo} from 'react';
-import ColorEditor from '../../components/color-editor';
-import {Ctx} from '../Style';
-import RenderSelect from './select';
-import {SelectOptions} from '../types';
-import GreyContainer from './greyContainer';
-import FontLayout from './fontLayout';
-import {FontFamilyOptions} from '../const';
-import { initValues } from '../utils';
-import {observe, useObservable} from '@mybricks/rxui';
+import React, { CSSProperties, useCallback, useMemo, useState } from "react";
+import {
+  Panel,
+  Select,
+  Toggle,
+  ColorEditor,
+  InputNumber,
+  FontSizeOutlined,
+  FontFamilyOutlined,
+  FontWeightOutlined,
+  TextAlignLeftOutlined,
+  TextAlignCenterOutlined,
+  TextAlignRightOutlined,
+  LetterSpacingOutlined,
+  LineHeightOutlined,
+  UnderLineOutlined,
+} from "../../style_new/StyleEditor/components";
+import { Ctx } from "../Style";
+// import RenderSelect from "./select";
+import { SelectOptions } from "../types";
+// import GreyContainer from "./greyContainer";
+// import FontLayout from "./fontLayout";
+import { FontFamilyOptions, OptionsMap } from "../const";
+import { initValues } from "../utils";
+import { observe, useObservable } from "@mybricks/rxui";
 
-import css from './index.less';
+// import css from "./index.less";
+import { splitValueAndUnit } from "../../style_new/StyleEditor/utils";
 
 const fontFamilyRegex =
   /font-family\s*?:(([^";<>]*?"[^";<>]*?")|(\s*[^";<>\s]*))*;?/g;
@@ -21,38 +37,65 @@ class EditCtx {
   fontWeight!: string;
   fontFamily!: string;
   lineHeight!: string;
+  textDecoration!: string;
   letterSpacing!: string;
   fontFamilyOptions!: SelectOptions;
 
   /** 布局相关 */
-  textAlign: CSSProperties['textAlign'];
-  display: CSSProperties['display'];
-  flexDirection: CSSProperties['flexDirection'];
-  alignItems: CSSProperties['alignItems'];
-  justifyContent: CSSProperties['justifyContent'];
+  textAlign: CSSProperties["textAlign"];
+  display: CSSProperties["display"];
+  flexDirection: CSSProperties["flexDirection"];
+  alignItems: CSSProperties["alignItems"];
+  justifyContent: CSSProperties["justifyContent"];
 
   projectData: any;
 }
 
-export const Font = function ({hasLetterSpace = false}) {
-  const ctx: Ctx = observe(Ctx, {from: 'parents'});
+const FONT_SIZE_OPTIONS = [
+  { label: "px", value: "px" },
+  { label: "继承", value: "inherit" },
+];
+
+const FONT_SIZE_DISABLED_LIST = ["inherit"];
+
+const LINEHEIGHT_UNIT_OPTIONS = [
+  { label: "倍数", value: "" },
+  { label: "px", value: "px" },
+  { label: "%", value: "%" },
+  { label: "继承", value: "inherit" },
+  { label: "默认", value: "normal" },
+];
+const LINEHEIGHT_UNIT_DISABLED_LIST = ["normal", "inherit"];
+const LETTERSPACING_UNIT_OPTIONS = [
+  { label: "px", value: "px" },
+  { label: "继承", value: "inherit" },
+  { label: "默认", value: "normal" },
+];
+const LETTERSPACING_UNIT_DISABLED_LIST = ["normal", "inherit"];
+
+export const Font = function ({ hasLetterSpace = false }) {
+  const ctx: Ctx = observe(Ctx, { from: "parents" });
   const editCtx: EditCtx = useObservable(EditCtx, (next) => {
     // const pubFontFamily = decodeURIComponent(ctx.projectData.pubFontFamily);
     // const fontFamilyAry = pubFontFamily.match(fontFamilyRegex);
-    const otherOptions: SelectOptions = (ctx.projectData.fontfaces || []).map((item: any) => {
-      const {label} = item
-      return {
-        label,
-        value: label
+    const otherOptions: SelectOptions = (ctx.projectData.fontfaces || []).map(
+      (item: any) => {
+        const { label } = item;
+        return {
+          label,
+          value: label,
+        };
       }
-    });
+    );
 
-    const defaultOptions: SelectOptions = (ctx.projectData.defaultFontfaces || []).map((item: any) => {
-      const {label} = item
+    const defaultOptions: SelectOptions = (
+      ctx.projectData.defaultFontfaces || []
+    ).map((item: any) => {
+      const { label } = item;
       return {
         label,
-        value: label
-      }
+        value: label,
+      };
     });
 
     // if (Array.isArray(fontFamilyAry)) {
@@ -66,16 +109,19 @@ export const Font = function ({hasLetterSpace = false}) {
     //   });
     // }
 
-    const nextValue = initValues({
-      lineHeight: '14px',
-      letterSpacing: '0px',
-      fontSize: '14px',
-      fontWeight: 400,
-      color: '#222222',
-      fontStyle: 'normal',
-      fontFamily: '',
-      textDecoration: 'normal'
-    }, ctx.val);
+    const nextValue = initValues(
+      {
+        lineHeight: "14px",
+        letterSpacing: "0px",
+        fontSize: "14px",
+        fontWeight: "400",
+        color: "#222222",
+        fontStyle: "normal",
+        fontFamily: "",
+        textDecoration: "normal",
+      },
+      ctx.val
+    );
 
     next({
       color: nextValue.color,
@@ -86,132 +132,193 @@ export const Font = function ({hasLetterSpace = false}) {
       fontFamily: nextValue.fontFamily,
       lineHeight: nextValue.lineHeight,
       letterSpacing: nextValue.letterSpacing,
-      fontFamilyOptions: otherOptions.concat(FontFamilyOptions).concat(defaultOptions),
+      fontFamilyOptions: otherOptions
+        .concat(FontFamilyOptions)
+        .concat(defaultOptions),
 
       textAlign: ctx.val.textAlign,
       display: ctx.val.display,
       flexDirection: ctx.val.flexDirection,
       alignItems: ctx.val.alignItems,
-      justifyContent: ctx.val.justifyContent
+      justifyContent: ctx.val.justifyContent,
     });
   });
 
+  const getTextAlignOptions = useCallback(() => {
+    const useStart = ["start", "end"].includes(editCtx.textAlign as any);
+
+    return [
+      {
+        label: <TextAlignLeftOutlined />,
+        value: useStart ? "start" : "left",
+        tip: "居左对齐",
+      },
+      { label: <TextAlignCenterOutlined />, value: "center", tip: "居中对齐" },
+      {
+        label: <TextAlignRightOutlined />,
+        value: useStart ? "end" : "right",
+        tip: "居右对齐",
+      },
+    ];
+  }, []);
+
+  const [lineHeight, setLineHeight] = useState<string | number>(
+    editCtx.lineHeight!
+  );
+
+  const onLineHeightChange = useCallback(
+    (lineHeight: string | number) => {
+      ctx.set({ lineHeight });
+      setLineHeight(lineHeight);
+    },
+    [lineHeight]
+  );
+
+  const onFontSizeChange = useCallback(
+    (fontSize: string | number) => {
+      const [fontSizeValue, fontSizeUnit] = splitValueAndUnit(fontSize);
+      const [_, lineHeightUnit] = splitValueAndUnit(lineHeight);
+
+      ctx.set({ fontSize });
+
+      if (fontSizeUnit === "px") {
+        const fontSizeNumber = Number(fontSizeValue);
+        const lineHeightNumber = fontSizeNumber + 8; // 根据fontSizeNumber需设置的行高
+        if (lineHeightUnit === "px") {
+          onLineHeightChange(`${lineHeightNumber}px`);
+        } else if (lineHeightUnit === "%") {
+          onLineHeightChange(
+            `${parseFloat(
+              ((lineHeightNumber * 100) / fontSizeNumber).toFixed(4)
+            )}%`
+          );
+        } else if (!isNaN(Number(lineHeight))) {
+          // parseFloat和toFixed保留四位小数并去除尾0 防止上下键无法增减
+          onLineHeightChange(
+            `${parseFloat((lineHeightNumber / fontSizeNumber).toFixed(4))}`
+          );
+        }
+      }
+    },
+    [lineHeight]
+  );
+
   const Render: JSX.Element = useMemo(() => {
     return (
-      <div className={css.editorContainer}>
-        <div className={css.editorTitle}>字体</div>
-        {ctx?.fontProps && ctx?.fontProps?.fontFamily !== false ?
-          <RenderSelect
-            options={editCtx.fontFamilyOptions}
-            defaultValue={editCtx.fontFamily}
-            onChange={(fontFamily) => {
-              ctx.set({fontFamily});
-            }}
-          />
-          :
-          <></>}
-        <div className={css.toolbar}>
-          <div className={css.item}>
-            <ColorEditor
-              value={editCtx.color}
-              onChange={(color: string) => {
-                ctx.set({color});
-              }}
-              style={{marginRight: 7, minWidth: 72, maxWidth: 72}}
+      <Panel title="字体">
+        {ctx?.fontProps && ctx?.fontProps?.fontFamily !== false && (
+          <Panel.Content>
+            <Select
+              tip={"字体"}
+              prefix={<FontFamilyOutlined />}
+              style={{ flexBasis: `100%`, padding: 0, overflow: "hidden" }}
+              defaultValue={editCtx.fontFamily}
+              options={editCtx.fontFamilyOptions}
+              onChange={(fontFamily: string) => ctx.set({ fontFamily })}
             />
-            <label className={css.label}>颜色</label>
-          </div>
-          <GreyContainer
-            label="粗体"
-            type="select"
-            optionsKey="fontWeight"
-            style={{marginRight: 7, flex: 1, cursor: 'pointer'}}
-            defaultValue={editCtx.fontWeight}
-            onChange={(fontWeight) => {
-              ctx.set({fontWeight});
+          </Panel.Content>
+        )}
+        <Panel.Content>
+          <ColorEditor
+            defaultValue={editCtx.color}
+            onChange={(color: string) => ctx.set({ color })}
+            style={{
+              flexBasis: `calc(66% - 3px)`,
+              padding: 0,
+              overflow: "hidden",
+              paddingLeft: 6,
             }}
           />
-          <GreyContainer
-            label="斜体"
-            type="select"
-            optionsKey="fontStyle"
-            style={{minWidth: 55, flex: 1, cursor: 'pointer'}}
-            defaultValue={editCtx.fontStyle}
-            onChange={(value) => {
-              ctx.set({fontStyle: value})
+          <Select
+            tip="粗细"
+            prefix={<FontWeightOutlined />}
+            style={{
+              flexBasis: `calc(33% - 3px)`,
+              padding: 0,
+              overflow: "hidden",
             }}
-          />
-        </div>
-        <div className={css.toolbar}>
-          <GreyContainer
-            type="input"
-            label="大小(px)"
-            onBlurFnKey=">0"
-            regexFnKey="number"
-            style={{marginRight: 7, minWidth: 72, maxWidth: 72}}
-            defaultValue={parseInt(editCtx.fontSize)}
-            onChange={(value) => {
-              const lineHeight =
-                parseInt(editCtx.lineHeight) -
-                parseInt(editCtx.fontSize) +
-                value +
-                'px';
-              const fontSize = value + 'px';
-              editCtx.lineHeight = lineHeight;
-              editCtx.fontSize = fontSize;
-              ctx.set({
-                lineHeight,
-                fontSize,
-              });
-            }}
-          />
-          <GreyContainer
-            label="行间距(px)"
-            type="input"
-            onBlurFnKey="default"
-            regexFnKey="number"
-            style={{marginRight: 7, flex: 1}}
             defaultValue={
-              //parseInt(editCtx.lineHeight) - parseInt(editCtx.fontSize)
-              parseInt(editCtx.lineHeight)
+              editCtx.fontWeight === "normal" ? "400" : editCtx.fontWeight
             }
-            onChange={(value) => {
-              //const lineHeight = parseInt(editCtx.fontSize) + Number(value) + 'px';
-              const lineHeight = Number(value) + 'px';
-              editCtx.lineHeight = lineHeight;
-              ctx.set({
-                lineHeight,
-              });
-            }}
+            options={OptionsMap["fontWeight"]}
+            onChange={(fontWeight) => ctx.set({ fontWeight })}
           />
-          <GreyContainer
-            label="下划线"
-            type="select"
-            optionsKey="textDecoration"
-            style={{minWidth: 55, flex: 1, cursor: 'pointer'}}
-            defaultValue={editCtx.textDecoration}
-            onChange={(value) => {
-              ctx.set({textDecoration: value})
-            }}
-          />
-        </div>
-        {hasLetterSpace ? (
-          <div className={css.toolbar}>
-            <GreyContainer
-              label="字间距(px)"
-              type="input"
-              onBlurFnKey="default"
-              regexFnKey="number"
-              style={{flex: 1}}
-              defaultValue={parseInt(editCtx.letterSpacing)}
-              onChange={(letterSpacing) => {
-                ctx.set({letterSpacing: letterSpacing + 'px'});
+          {
+            // TODO 加上斜体会因为 ColorEditor 太长导致变挤
+          }
+          {/* <Select
+              tip="斜体"
+              prefix={<FontWeightOutlined />}
+              style={{
+                flexBasis: `calc(33% - 3px)`,
+                padding: 0,
+                overflow: "hidden",
               }}
+              defaultValue={String(editCtx.fontStyle)}
+              options={OptionsMap["fontStyle"]}
+              onChange={(fontStyle) => ctx.set({ fontStyle })}
+            /> */}
+        </Panel.Content>
+        <Panel.Content>
+          <InputNumber
+            tip="大小"
+            style={{ flexBasis: `calc(33% - 3px)` }}
+            prefix={<FontSizeOutlined />}
+            defaultValue={editCtx.fontSize}
+            defaultUnitValue="px"
+            unitOptions={FONT_SIZE_OPTIONS}
+            unitDisabledList={FONT_SIZE_DISABLED_LIST}
+            onChange={onFontSizeChange}
+          />
+          <InputNumber
+            tip="行高"
+            style={{ flexBasis: `calc(33% - 3px)` }}
+            prefix={<LineHeightOutlined />}
+            value={editCtx.lineHeight}
+            defaultUnitValue="inherit"
+            unitOptions={LINEHEIGHT_UNIT_OPTIONS}
+            unitDisabledList={LINEHEIGHT_UNIT_DISABLED_LIST}
+            onChange={onLineHeightChange}
+          />
+          <Select
+            tip="下划线"
+            prefix={<UnderLineOutlined />}
+            style={{
+              flexBasis: `calc(33% - 3px)`,
+              padding: 0,
+              overflow: "hidden",
+            }}
+            defaultValue={editCtx.textDecoration}
+            options={OptionsMap["textDecoration"]}
+            onChange={(textDecoration) => ctx.set({ textDecoration })}
+          />
+        </Panel.Content>
+        {hasLetterSpace && (
+          <Panel.Content>
+            <InputNumber
+              tip="间距"
+              style={{ flexBasis: `calc(33% - 3px)` }}
+              prefix={<LetterSpacingOutlined />}
+              defaultValue={editCtx.letterSpacing}
+              unitOptions={LETTERSPACING_UNIT_OPTIONS}
+              unitDisabledList={LETTERSPACING_UNIT_DISABLED_LIST}
+              onChange={(letterSpacing) => ctx.set({ letterSpacing })}
             />
-          </div>
-        ) : null}
-        <div style={{ width: '100%' }}>
-          <FontLayout
+          </Panel.Content>
+        )}
+        <Panel.Content>
+          <Toggle
+            defaultValue={editCtx.textAlign}
+            options={getTextAlignOptions()}
+            onChange={(textAlign) => {
+              ctx.set({ textAlign });
+            }}
+          />
+        </Panel.Content>
+        {
+          // TODO 垂直相关
+        }
+        {/* <FontLayout
             verticalAlign={ctx.fontProps?.verticalAlign ?? true}
             horizontalAlign={ctx.fontProps.horizontalAlign ?? true}
             value={{
@@ -224,13 +331,12 @@ export const Font = function ({hasLetterSpace = false}) {
             onChange={(nextVal) => {
               ctx.set({ ...(nextVal ?? {}) });
             }}
-          />
-        </div>
-      </div>
+          /> */}
+      </Panel>
     );
   }, [hasLetterSpace]);
 
   return Render;
 };
 
-export default () => <Font hasLetterSpace={false}/>;
+export default () => <Font hasLetterSpace={false} />;
--- src/style/const.ts
+++ src/style/const.ts
@@ -1,26 +1,39 @@
-import {AnyMap, SelectOptions} from './types';
+import { AnyMap, SelectOptions } from "./types";
 
 // font-family选择项
 export const FontFamilyOptions: SelectOptions = [
-  {label: '默认', value: ''},
-  {
-    label: 'system-ui',
-    value:
-      "system-ui, -apple-system, BlinkMacSystemFont, 'PingFang SC', miui, Roboto, Helvetica Neue, Helvetica, sans-serif",
-  },
-  {label: 'din', value: 'DIN'},
-  {label: 'AvenirNext', value: 'AvenirNext, AvenirNext-BoldItalic'},
+  { label: "默认", value: "" },
+  { label: "继承", value: "inherit" },
+  { label: "din", value: "DIN" },
+  { label: "AvenirNext", value: "AvenirNext, AvenirNext-BoldItalic" },
+  { label: "PingFang SC", value: "PingFang SC" },
+  { label: "Microsoft YaHei", value: "Microsoft YaHei" },
+  { label: "微软雅黑", value: "微软雅黑" },
+  { label: "Arial", value: "Arial" },
+  { label: "sans-serif", value: "sans-serif" },
+  { label: "Helvetica Neue", value: "Helvetica Neue" },
+  { label: "Helvetica", value: "Helvetica" },
+  { label: "Hiragino Sans GB", value: "Hiragino Sans GB" },
+  { label: "-apple-system", value: "-apple-system" },
+  { label: "BlinkMacSystemFont", value: "BlinkMacSystemFont" },
+  { label: "Segoe UI", value: "Segoe UI" },
+  { label: "Roboto", value: "Roboto" },
+  { label: "Noto Sans", value: "Noto Sans" },
+  { label: "Apple Color Emoji", value: "Apple Color Emoji" },
+  { label: "Segoe UI Emoji", value: "Segoe UI Emoji" },
+  { label: "Segoe UI Symbol", value: "Segoe UI Symbol" },
+  { label: "Noto Color Emoji", value: "Noto Color Emoji" },
 ];
 
 // font-weight选择项
 export const FontWeightOptions: SelectOptions = [
-  {label: '极细', value: '100'},
-  {label: '特细', value: '200'},
-  {label: '细', value: '300'},
-  {label: '标准', value: '400'},
-  {label: '中黑', value: '500'},
-  {label: '中粗', value: '700'},
-  {label: '特粗', value: '900'},
+  { label: "极细", value: "100" },
+  { label: "特细", value: "200" },
+  { label: "细", value: "300" },
+  { label: "标准", value: "400" },
+  { label: "中黑", value: "500" },
+  { label: "中粗", value: "700" },
+  { label: "特粗", value: "900" },
   // { label: '100', value: '100' },
   // { label: '200', value: '200' },
   // { label: '300', value: '300' },
@@ -30,31 +43,31 @@ export const FontWeightOptions: SelectOptions = [
   // { label: '700', value: '700' },
   // { label: '800', value: '800' },
   // { label: '900', value: '900' },
-  // {label: 'bold', value: 'bold'},
-  // {label: 'bolder', value: 'bolder'},
-  // {label: 'lighter', value: 'lighter'},
-  // {label: 'normal', value: 'normal'}
+  // { label: "bold", value: "bold" },
+  // { label: "bolder", value: "bolder" },
+  // { label: "lighter", value: "lighter" },
+  // { label: "normal", value: "normal" },
 ];
 
 // font-style选择项
 export const FontStyleOptions: SelectOptions = [
-  {label: '默认', value: 'normal'},
-  {label: '斜体', value: 'italic'}
+  { label: "默认", value: "normal" },
+  { label: "斜体", value: "italic" },
 ];
 
 export const TextDecoratioin: SelectOptions = [
-  {label: '默认', value: 'normal'},
-  {label: '下划线', value: 'underline'},
-  {label: '上划线', value: 'overline'},
-  {label: '中划线', value: 'line-through'},
+  { label: "默认", value: "normal" },
+  { label: "下划线", value: "underline" },
+  { label: "上划线", value: "overline" },
+  { label: "中划线", value: "line-through" },
   // {label: '闪烁', value: 'blink'},
 ];
 
 // border-style选择项
 export const BorderStyleOptions: SelectOptions = [
-  {label: '实线', value: 'solid'},
-  {label: '虚线', value: 'dashed'},
-  {label: '无', value: 'none'},
+  { label: "实线", value: "solid" },
+  { label: "虚线", value: "dashed" },
+  { label: "无", value: "none" },
   // {label: 'hidden', value: 'hidden'},
   // {label: 'dotted', value: 'dotted'},
   // {label: 'double', value: 'double'},
@@ -66,58 +79,58 @@ export const BorderStyleOptions: SelectOptions = [
 
 // background-size选择项
 export const BackgroundSizeOptions: SelectOptions = [
-  {label: '适应', value: 'contain'},
-  {label: '填充', value: 'cover'},
-  {label: '铺满', value: '100% 100%'},
-  {label: '铺满x轴', value: '100% auto'},
-  {label: '铺满y轴', value: 'auto 100%'},
+  { label: "适应", value: "contain" },
+  { label: "填充", value: "cover" },
+  { label: "铺满", value: "100% 100%" },
+  { label: "铺满x轴", value: "100% auto" },
+  { label: "铺满y轴", value: "auto 100%" },
 ];
 
 // background-repeat选择项
 export const BackgroundRepeatOptions: SelectOptions = [
-  {label: '平铺', value: 'repeat'},
-  {label: '不平铺', value: 'no-repeat'},
+  { label: "平铺", value: "repeat" },
+  { label: "不平铺", value: "no-repeat" },
 ];
 
 // background-position选择项
 export const BackgroundPositionOptions: SelectOptions = [
-  {label: '居上', value: 'center top'},
-  {label: '居中', value: 'center center'},
-  {label: '居下', value: 'center bottom'},
-  {label: '居左', value: 'left center'},
-  {label: '居右', value: 'right center'},
-  {label: '左上', value: 'left top'},
-  {label: '左下', value: 'left bottom'},
-  {label: '右上', value: 'right top'},
-  {label: '右下', value: 'right bottom'},
+  { label: "居上", value: "center top" },
+  { label: "居中", value: "center center" },
+  { label: "居下", value: "center bottom" },
+  { label: "居左", value: "left center" },
+  { label: "居右", value: "right center" },
+  { label: "左上", value: "left top" },
+  { label: "左下", value: "left bottom" },
+  { label: "右上", value: "right top" },
+  { label: "右下", value: "right bottom" },
 ];
 
 // 默认配色
 export const presetColors: Array<string> = [
-  'rgb(245, 34, 45)',
-  'rgb(250, 84, 28)',
-  'rgb(250, 140, 22)',
-  'rgb(250, 173, 20)',
-  'rgb(250, 219, 20)',
-  'rgb(160, 217, 17)',
-  'rgb(82, 196, 26)',
-  'rgb(19, 194, 194)',
-  'rgb(24, 144, 255)',
-  'rgb(47, 84, 235)',
-  'rgb(114, 46, 209)',
-  'rgb(235, 47, 150)',
-  'rgb(255, 77, 79)',
-  'rgb(255, 122, 69)',
-  'rgb(255, 169, 64)',
-  'rgb(255, 197, 61)',
-  'rgb(255, 236, 61)',
-  'rgb(186, 230, 55)',
-  'rgb(115, 209, 61)',
-  'rgb(54, 207, 201)',
-  'rgb(64, 169, 255)',
-  'rgb(89, 126, 247)',
-  'rgb(255 255 255)',
-  'rgba(255, 255, 255, 0)',
+  "rgb(245, 34, 45)",
+  "rgb(250, 84, 28)",
+  "rgb(250, 140, 22)",
+  "rgb(250, 173, 20)",
+  "rgb(250, 219, 20)",
+  "rgb(160, 217, 17)",
+  "rgb(82, 196, 26)",
+  "rgb(19, 194, 194)",
+  "rgb(24, 144, 255)",
+  "rgb(47, 84, 235)",
+  "rgb(114, 46, 209)",
+  "rgb(235, 47, 150)",
+  "rgb(255, 77, 79)",
+  "rgb(255, 122, 69)",
+  "rgb(255, 169, 64)",
+  "rgb(255, 197, 61)",
+  "rgb(255, 236, 61)",
+  "rgb(186, 230, 55)",
+  "rgb(115, 209, 61)",
+  "rgb(54, 207, 201)",
+  "rgb(64, 169, 255)",
+  "rgb(89, 126, 247)",
+  "rgb(255 255 255)",
+  "rgba(255, 255, 255, 0)",
 ];
 
 export const OptionsMap = {
@@ -133,66 +146,66 @@ export const OptionsMap = {
 
 // 默认宽高样式
 export const DefaultSize = {
-  width: '100%',
-  height: '100%',
+  width: "100%",
+  height: "100%",
 };
 
 // 默认高样式
 export const DefaultHeight = {
-  height: '100%',
+  height: "100%",
 };
 
 // 默认宽样式
 export const DefaultWidth = {
-  width: '100%',
+  width: "100%",
 };
 
 // 默认字体样式
 export const DefaultFont: AnyMap = {
-  lineHeight: '14px',
-  letterSpacing: '0px',
-  fontSize: '14px',
+  lineHeight: "14px",
+  letterSpacing: "0px",
+  fontSize: "14px",
   fontWeight: 400,
-  color: '#222222',
-  fontStyle: 'normal',
-  fontFamily: '',
+  color: "#222222",
+  fontStyle: "normal",
+  fontFamily: "",
 };
 
 // 默认边框样式
 export const DefaultBorder: AnyMap = {
-  borderWidth: '0px',
-  borderRadius: '0px',
-  borderStyle: 'solid',
-  borderColor: '#ffffff',
+  borderWidth: "0px",
+  borderRadius: "0px",
+  borderStyle: "solid",
+  borderColor: "#ffffff",
 };
 
 // 背景默认样式 包括背景色、背景图
 export const DefaultBg: AnyMap = {
-  background: 'url() center top / 100% 100% no-repeat, rgba(255,255,255,1)'
+  background: "url() center top / 100% 100% no-repeat, rgba(255,255,255,1)",
 };
 
 // 背景图片默认样式
 export const DefaultBgImage: AnyMap = {
-  backgroundImage: 'url()',
-  backgroundSize: '100% 100%',
-  backgroundPosition: 'center top',
-  backgroundRepeat: 'no-repeat',
+  backgroundImage: "url()",
+  backgroundSize: "100% 100%",
+  backgroundPosition: "center top",
+  backgroundRepeat: "no-repeat",
 };
 
 // 内间距默认样式
 export const DefaultPadding: AnyMap = {
-  paddingTop: '0px',
-  paddingBottom: '0px',
-  paddingLeft: '0px',
-  paddingRight: '0px',
+  paddingTop: "0px",
+  paddingBottom: "0px",
+  paddingLeft: "0px",
+  paddingRight: "0px",
 };
 
 export const DefaultBoxShaodw: AnyMap = {
-  boxShadow: '0px 0px 0px 0px #000000',
+  boxShadow: "0px 0px 0px 0px #000000",
 };
 
 export const DefaultTextShaodw: AnyMap = {
-  boxShadow: '0px 0px 0px #000000',
+  boxShadow: "0px 0px 0px #000000",
 };
 
 export const DefaultConfigMap: { [key: string]: AnyMap } = {
@@ -209,29 +222,29 @@ export const DefaultConfigMap: { [key: string]: AnyMap } = {
 };
 
 export const Sequence: Array<string> = [
-  'PADDING',
-  'SIZE',
+  "PADDING",
+  "SIZE",
   // 'WIDTH',
   // 'HEIGHT',
-  'FONT',
-  'ALIGN',
-  'TEXTSHADOW',
-  'BORDER',
-  'BGCOLOR',
-  'BGIMAGE',
-  'SHADOW',
+  "FONT",
+  "ALIGN",
+  "TEXTSHADOW",
+  "BORDER",
+  "BGCOLOR",
+  "BGIMAGE",
+  "SHADOW",
 ];
 
 export const TitleMap: { [key: string]: string } = {
-  SIZE: '尺寸',
-  PADDING: '内间距',
+  SIZE: "尺寸",
+  PADDING: "内间距",
   // 'WIDTH': '宽',
   // 'HEIGHT': '高',
-  FONT: '字体',
-  ALIGN: '对齐',
-  BORDER: '边框',
-  BGCOLOR: '背景色',
-  BGIMAGE: '背景图',
-  SHADOW: '阴影',
-  TEXTSHADOW: '文字阴影',
+  FONT: "字体",
+  ALIGN: "对齐",
+  BORDER: "边框",
+  BGCOLOR: "背景色",
+  BGIMAGE: "背景图",
+  SHADOW: "阴影",
+  TEXTSHADOW: "文字阴影",
 };
