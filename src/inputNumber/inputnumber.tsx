5daff7135091c57dd2e8489645ac2fed6014c947
chemingjun <chemingjun@126.com>
2025-02-16 18:26:58 +0800

    inputNumber isNaN

--- src/inputNumber/inputnumber.tsx
+++ src/inputNumber/inputnumber.tsx
@@ -8,15 +8,15 @@ import {editorsConfigKey} from '../constant';
 export default function ({editConfig}): any {
   const {value, options = []} = editConfig;
   const model = useObservable({val: null, value}, [value]);
-  
+
   // let resAry: any[] = deepCopy(model.val)
-  
+
   // const update = useCallback(() => {
   //   model.val = resAry
   //   model.value.set(resAry)
   // }, [resAry])
   //const ffn = value.get
-  
+
   return <Fn options={options} model={model}/>;
 }
 
@@ -39,7 +39,7 @@ function Fn({options, model}) {
               size: 'small' | 'middle' | 'large' | undefined;
             };
             const item = Object.assign(defaultConfig, other);
-            
+
             return (
               <Item
                 key={index}
@@ -63,11 +63,11 @@ function Item({index, model, formatter, item, width}) {
   useComputed(() => {
     model.val = Array.isArray(model.value.get()) ? model.value.get() : [0];
   });
-  
+
   const update = useCallback(() => {
     model.value.set(model.val);
   }, []);
-  
+
   // useEffect(()=>{
   //   return ()=>{
   //     console.log(Math.random())
@@ -76,12 +76,17 @@ function Item({index, model, formatter, item, width}) {
 
   const updateValue = (e: React.FocusEvent<HTMLInputElement, Element>) => {
     const curVal = parseInt(e.target.value)
-            
+
     if (typeof curVal === 'number') {
-      if (curVal >= item.min && curVal <= item.max) {
-        model.val[index] = curVal
-        
+      if (isNaN(curVal)) {
+        model.val[index] = 0
         update()
+      } else {
+        if (curVal >= item.min && curVal <= item.max) {
+          model.val[index] = curVal
+
+          update()
+        }
       }
     } else {
       if (!curVal) {
@@ -90,7 +95,7 @@ function Item({index, model, formatter, item, width}) {
       }
     }
   }
-  
+
   return (
     <div className={css.editInputnumber}>
       <div className={css.editInputnumberAll} style={{width}}>
@@ -124,7 +129,7 @@ function Item({index, model, formatter, item, width}) {
           //   //   update();
           //   // }
           // }}
-          
+
           onBlur={updateValue}
         />
         <div className={css.editInputnumberAllTitle}>{item.title || ''}</div>
