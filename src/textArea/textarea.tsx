234d9c65bae14124dd95a29dcecbba7e0471f44a
chemingjun <chemingjun@126.com>
2025-04-03 20:24:00 +0800

    ext for textarea

--- src/textArea/textarea.tsx
+++ src/textArea/textarea.tsx
@@ -1,15 +1,18 @@
-import React, { useMemo, useCallback, useEffect, useRef, useState } from "react";
-import { world } from './icons'
-import { Input } from "antd";
-import { debounce } from "../util/lodash";
-import { EditorProps } from "../interface";
-import { useObservable } from "@mybricks/rxui";
-import { getOptionsFromEditor, isValid } from "../utils";
+import React, {useMemo, useCallback, useEffect, useRef, useState} from "react";
+import {world} from './icons'
+import {Input} from "antd";
+import {debounce} from "../util/lodash";
+import {EditorProps} from "../interface";
+import {useObservable} from "@mybricks/rxui";
+import {getOptionsFromEditor, isValid} from "../utils";
 
 import css from "./index.less";
 
-export default function ({ editConfig }: EditorProps): JSX.Element {
-  const { value, options = {}, locales } = editConfig;
+export default function ({editConfig}: EditorProps): JSX.Element {
+  const {
+    value, options = {}, locales
+  } = editConfig;
+
   const [curText, setCurText] = useState('')
   const [useLocale, setUseLocale] = useState(false)
 
@@ -19,16 +22,18 @@ export default function ({ editConfig }: EditorProps): JSX.Element {
     style = {
       height: 100,
     },
+    width,
+    height,
+    focusNow,
+    selectContent,
     locale = false,
     ...res
   } = getOptionsFromEditor(options);
   const localeEnable = !!locales && !!locale
 
   const styles = useMemo(() => {
-    const { height = 100 } = style;
-
     return {
-      height,
+      height: height || style.height || 100,
     };
   }, []);
 
@@ -87,6 +92,21 @@ export default function ({ editConfig }: EditorProps): JSX.Element {
     }
   }, [])
 
+
+  useEffect(() => {
+    if (changedRef.current) {
+      if (focusNow) {
+        changedRef.current.focus()
+        if (selectContent) {
+          changedRef.current.select()
+        }
+      }
+    }
+
+  }, [
+    changedRef.current
+  ])
+
   useEffect(() => {
     return () => {
       if (useLocale && changedRef.current) {
@@ -97,29 +117,37 @@ export default function ({ editConfig }: EditorProps): JSX.Element {
   }, [useLocale])
 
   return (
-    <div className={css["editor-textArea"]}>
-      <Input.TextArea
-        style={styles}
-        onChange={evt => {
-          setCurText(evt.target.value)
-        }}
+    <div className={css["editor-textArea"]}
+         style={{
+           width,
+           height
+         }}>
+      <Input.TextArea ref={el => {
+        if (el) {
+          changedRef.current = el
+        }
+      }}
+                      style={styles}
+                      onChange={evt => {
+                        setCurText(evt.target.value)
+                      }}
         // onKeyPress={evt => {
         //   if (evt.key !== 'Enter') return
         //   updateVal(evt)
         // }}
-        onBlur={(evt) => {
-          updateVal(evt)
-        }}
-        disabled={readonly || useLocale}
-        defaultValue={val}
-        {...res}
-        value={curText}
+                      onBlur={(evt) => {
+                        updateVal(evt)
+                      }}
+                      disabled={readonly || useLocale}
+                      defaultValue={val}
+                      {...res}
+                      value={curText}
       />
       {
         localeEnable ? (
           <span className={`${useLocale ? css.useLocale : ''} ${css.icon}`}
-            onClick={openLocale}
-            data-mybricks-tip={`多语言`}>
+                onClick={openLocale}
+                data-mybricks-tip={`多语言`}>
             {world}
           </span>
         ) : null
