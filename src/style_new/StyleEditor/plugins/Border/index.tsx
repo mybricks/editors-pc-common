f00c2be0215c2fd39731ffcde59a2caaf5b39d84
moyukai05 <moyukai05@kuaishou.com>
2025-06-24 14:50:38 +0800

    fix:展开后的边框编辑器样式

--- src/style_new/StyleEditor/plugins/Border/index.tsx
+++ src/style_new/StyleEditor/plugins/Border/index.tsx
@@ -281,7 +281,7 @@ export function Border({ value, onChange, config, showTitle, collapse }: BorderP
           <div className={css.col}>
             {!disableBorderTop && (
               <div className={css.row}>
-                <Panel.Content style={{ padding: 3 }}>
+                <Panel.Content style={{ padding: 3, minWidth:120 }}>
                   <Panel.Item
                     className={css.editArea}
                     style={{ padding: "0px 8px" }}
@@ -363,7 +363,7 @@ export function Border({ value, onChange, config, showTitle, collapse }: BorderP
 
             {!disableBorderRight && (
               <div className={css.row}>
-                <Panel.Content style={{ padding: 3 }}>
+                <Panel.Content style={{ padding: 3, minWidth:120 }}>
                   <Panel.Item
                     className={css.editArea}
                     style={{ padding: "0px 8px" }}
@@ -434,7 +434,7 @@ export function Border({ value, onChange, config, showTitle, collapse }: BorderP
 
             {!disableBorderBottom && (
               <div className={css.row}>
-                <Panel.Content style={{ padding: 3 }}>
+                <Panel.Content style={{ padding: 3, minWidth:120 }}>
                   <Panel.Item
                     className={css.editArea}
                     style={{ padding: "0px 8px" }}
@@ -505,7 +505,7 @@ export function Border({ value, onChange, config, showTitle, collapse }: BorderP
 
             {!disableBorderLeft && (
               <div className={css.row}>
-                <Panel.Content style={{ padding: 3 }}>
+                <Panel.Content style={{ padding: 3, minWidth:120 }}>
                   <Panel.Item
                     className={css.editArea}
                     style={{ padding: "0px 8px" }}
