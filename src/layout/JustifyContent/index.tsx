e165027a34f0c93f37867f5d6a89f1ccc5480e3c
moyukai05 <moyukai05@kuaishou.com>
2025-07-16 17:33:11 +0800

    fix:修复布局编辑器换行联动关系

--- src/layout/JustifyContent/index.tsx
+++ src/layout/JustifyContent/index.tsx
@@ -73,7 +73,7 @@ export default ({
             {render(flexDirection)}
           </div>
         ))}
-          {renderWrap()}
+          {flexDirection === "row" && renderWrap()}
       </div>
     </>
   );
